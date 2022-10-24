83b66ff650abaac4ae14522157c4fce8e30e2fde
udarenienao <72667318+udarenienao@users.noreply.github.com>
2022-10-24 17:50:59 +0500

    add tabs

--- project/src/pages/movie-page/movie-page.tsx
+++ project/src/pages/movie-page/movie-page.tsx
@@ -5,6 +5,7 @@ import Catalog from '../../components/catalog/catalog';
 import {Film} from '../../types/film';
 import Logo from '../../components/logo/logo';
 import {Link, Navigate, useParams} from 'react-router-dom';
+import FilmDescription from '../../components/film-description/film-description';
 
 
 type MoviePageProps = {
@@ -85,39 +86,7 @@ function MoviePage({ films }: MoviePageProps): JSX.Element{
                 width="218" height="327"
               />
             </div>
-
-            <div className="film-card__desc">
-              <nav className="film-nav film-card__nav">
-                <ul className="film-nav__list">
-                  <li className="film-nav__item film-nav__item--active">
-                    <a href="#" className="film-nav__link">Overview</a>
-                  </li>
-                  <li className="film-nav__item">
-                    <a href="#" className="film-nav__link">Details</a>
-                  </li>
-                  <li className="film-nav__item">
-                    <a href="#" className="film-nav__link">Reviews</a>
-                  </li>
-                </ul>
-              </nav>
-
-              <div className="film-rating">
-                <div className="film-rating__score">{film.rating}</div>
-                <p className="film-rating__meta">
-                  <span className="film-rating__level">{getRatingLevel(film.rating)}</span>
-                  <span className="film-rating__count">{`${film.scoresCount} ratings`}</span>
-                </p>
-              </div>
-
-              <div className="film-card__text">
-                <p>{film.description}</p>
-
-                <p className="film-card__director"><strong>Director: {film.director}</strong></p>
-
-                <p className="film-card__starring"><strong>Starring: {film.starring.join(', ')}</strong>
-                </p>
-              </div>
-            </div>
+            <FilmDescription film={film}/>
           </div>
         </div>
       </section>
@@ -136,19 +105,3 @@ function MoviePage({ films }: MoviePageProps): JSX.Element{
 
 export default MoviePage;
 
-
-function getRatingLevel(rating: number): string{
-  if (rating < 3) {
-    return 'Bad';
-  }
-  if (rating < 5) {
-    return 'Normal';
-  }
-  if (rating < 8) {
-    return 'Good';
-  }
-  if (rating < 10) {
-    return 'Very good';
-  }
-  return 'Awesome';
-}
